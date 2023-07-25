87eb9d3c7bc65daab7dff5be2997f12dcf916939
Aditya Gunda <adityagunda@Adityas-MacBook-Pro-2.local>
2023-07-24 23:17:44 -0700

    added benchmark uploader link

--- streamlit.py
+++ streamlit.py
@@ -5,6 +5,7 @@ from ehr_handlers import handle_selected_ehr
 
 st.title('Automated Revenue Benchmark Generator')
 st.write('This tool only helps generate a benchmark. It does NOT upload your benchmark for you.')
+st.write('Please upload your benchmark here afterwards: https://athelas.retool.com/apps/784f8256-1898-11ee-8939-2f7149adaf6a/Revenue%20Ops%20Benchmark%20Uploader')
 
 ehr_option = st.selectbox(
     'Select an EHR',
